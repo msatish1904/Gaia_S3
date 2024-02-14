ba732479a1f454a59cb1b1f553dab5e50fb2fe7d
msatish1904 <99471611+msatish1904@users.noreply.github.com>
2024-02-14 10:18:06 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,8 @@
 # Configure the AWS provider
 provider "aws" {
   region = "eu-west-1"
+aws_access_key = "AKIAW3MEFAIS2WPS72WH"
+aws_secret_key = "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"
 }
 
 # Create a S3 bucket
