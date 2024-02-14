e117d421d765cca7f90d94b7f9ff494d7f037244
msatish1904 <99471611+msatish1904@users.noreply.github.com>
2024-02-14 10:23:13 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,9 @@
 # Configure the AWS provider
-provider "aws" 
-{
+provider "aws" {
    region = "us-east-1"
    aws_access_key = "AKIAW3MEFAIS4V46K4SZ"
    aws_secret_key = "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"
 }
-
 # Create a S3 bucket
 resource "aws_s3_bucket" "terraform_state" {
   bucket		  = "${var.bucket_name}"
